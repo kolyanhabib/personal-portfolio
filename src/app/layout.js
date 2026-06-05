a4399f30a39cb152363076cfdd82b3339ad23713
kolyanhabib <kolyanhabib@gmail.com>
2026-06-05 12:49:16 +0600

    updated the google font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,21 @@
 import "./globals.css";
 
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
+
 export const metadata = {
-  title: "Kolyan Habib Portfolio",
+  title: "Kolyan Habib - Portfolio",
   description: "Professional Portfolio",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.variable}>
       <body>{children}</body>
     </html>
   );
